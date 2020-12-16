368825b2a6707cd4025eeec572f11b0de0e7bf0e
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-12-16 00:28:13 +0000

    More mongodb palyground changes

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,15 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+const { MongoClient, ObjectID} = require('mongodb')
+
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -11,8 +18,30 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Sunny', 
-        age: 27
-    })
-})
+    // db.collection('users').insertOne({
+    //     name: 'Vikram', 
+    //     age: 28
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+//     db.collection('users').insertMany([
+//         {
+//             name: 'Jen', 
+//             age: 28
+//         }, {
+//             name: 'Gunther', 
+//             age: 27
+//         }
+//     ], (error, result) => {
+//         if (error) {
+//             return console.log('Unable to insert documents')
+//         }
+
+//         console.log(result.ops)
+//     })
+ })
